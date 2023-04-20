d0d37d06cab384e6abaacc1fce7a6e6b5121b45d
brandonwmichael <brandon@bwmlab.com>
2023-04-20 08:41:37 -0500

    fix

--- src/formatmyname/format_name.py
+++ src/formatmyname/format_name.py
@@ -4,9 +4,3 @@ def format_name(first_name, last_name):
     full_name = f'{first_name} {last_name}'
     # full_name = f'{first_name}'
     return full_name
-
-def main():
-    assert format_name('John', 'Doe') == 'John Doe'
-    
-if __name__ == '__main__':
-    main()
