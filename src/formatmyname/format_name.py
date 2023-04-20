cd63e88ab2583f7c67bf9922c8d4a16434e8590f
brandonwmichael <brandon@bwmlab.com>
2023-04-20 08:53:16 -0500

    check_name

--- src/formatmyname/format_name.py
+++ src/formatmyname/format_name.py
@@ -4,3 +4,9 @@ def format_name(first_name, last_name):
     full_name = f'{first_name} {last_name}'
     # full_name = f'{first_name}'
     return full_name
+
+
+def check_name(first_name, last_name):
+    full_name = f'[OK]{first_name} {last_name}'
+    # full_name = f'{first_name}'
+    return full_name
